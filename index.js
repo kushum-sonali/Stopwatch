8b49e4efa40b189ff56f9bffa269c4d080b7dc05
kushum-sonali <chsonali77@gmail.com>
2022-09-28 12:55:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const abc=document.getElementsByClassName("watch")[0];
+
+let hr=0;
+let min=0;
+let sec=0;
+let stoptime=true;
+
+function startTimer(){
+    // console.log("hello");
+
+if(stoptime==true){
+    // console.log("hello");
+
+stoptime==false;
+sec=parseInt(sec);
+min=parseInt(min);
+hr=parseInt(hr);
+sec=sec+1;
+// console.log("hello");
+
+if(sec==60){
+    min=min+1;
+    // console.log("hello");
+
+    sec=0;
+}
+if(min==60){
+    hr=hr+1;
+    min=0;
+    sec=0;
+}
+if(sec<10){
+    sec="0"+sec;
+}
+if(min<10){
+    min="0"+min;
+}
+if(hr<10){
+    hr="0"+hr;
+}
+abc.innerHTML=hr+":"+ min + ":" + sec
+setTimeout("startTimer()",1000);
+}
+// setTimeout("timercycle()",1000);
+// timercycle();
+// }  console.log("hello");
+
+}
+function stopTime(){
+//   if(stoptime==true){
+    // console.log("hello");
+//   }
+    if(stoptime=false){
+    console.log("hello");
+    stoptime==true;
+
+    }
+}
+// function timercycle(){
+    
+//    if(stoptime==false){
+    // console.log("hello");
+    // sec=parseInt(sec);
+    // min=parseInt(min);
+    // hr=parseInt(hr);
+    // sec=sec+1;
+    
+    // if(sec==60){
+    //     min=min+1;
+    //     sec=0;
+    // }
+    // if(min==60){
+    //     hr=hr+1;
+    //     min=0;
+    //     sec=0;
+    // }
+    // if(sec<10){
+    //     sec="0"+sec;
+    // }
+    // if(min<10){
+    //     min="0"+min;
+    // }
+    // if(hr<10){
+    //     hr="0"+hr;
+    // }
+    // abc.innerHTML=hr+":"+ min + ":" + sec
+    // setTimeout("timercycle()",1000);
+// }
+// }
+
+function resetTimer(){
+    // console.log("hello");
+    
+abc.innerHTML="00:00:00";
+console.log("hello");
+
+hr=0;
+min=0;
+sec=0;
+stopTime();
+startTimer();
+}
